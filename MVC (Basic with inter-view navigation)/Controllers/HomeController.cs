3a24b4155905683c4a8b2043e804f69d87b8b84a
DESKTOP-P58U6P6\Talamino <csi21-atallez@colegioaltair.net>
2022-11-14 11:47:18 +0100

    Add project files.

---
+++ MVC (Basic with inter-view navigation)/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC__Basic_with_inter_view_navigation_.Models;
+using System.Diagnostics;
+
+namespace MVC__Basic_with_inter_view_navigation_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View(ListUsers());
+        }
+
+        public static List<string> ListUsers()
+        {
+            List<string> ListUsers = new List<string>();
+            ListUsers.Add("Fernando");
+            ListUsers.Add("Alejandra");
+            ListUsers.Add("Raul");
+            ListUsers.Add("Martin");
+            ListUsers.Add("Ernesto");
+            return ListUsers;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC (Basic with inter-view navigation)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC__Basic_with_inter_view_navigation_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
